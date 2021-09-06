16d34fce9ccc9d385f8d122b8fb8b3df599be6d3
tldjfj123 <dksckd0804@naver.com>
2021-09-06 12:57:00 +0900

    21.09.06

---
+++ 10773.py
@@ -0,0 +1,22 @@
+"""
+첫 번째 줄에 정수 K가 주어진다. (1 ≤ K ≤ 100,000)
+
+이후 K개의 줄에 정수가 1개씩 주어진다. 정수는 0에서 1,000,000 사이의 값을 가지며, 정수가 "0" 일 경우에는 가장 최근에 쓴 수를 지우고, 아닐 경우 해당 수를 쓴다.
+
+정수가 "0"일 경우에 지울 수 있는 수가 있음을 보장할 수 있다.
+"""
+import sys
+
+
+num = int(sys.stdin.readline())
+
+items = []
+
+for _ in range(num) :
+    i = int(sys.stdin.readline())
+    if i == 0 :
+        items.pop()
+        continue
+    items.append(i)
+
+print(sum(items))
