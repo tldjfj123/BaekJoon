ea2780732554c75d448a4b7ef28f9df43f26919d
tldjfj123 <dksckd0804@naver.com>
2021-10-24 11:11:44 +0900

    21.10.24

---
+++ 19947.py
@@ -0,0 +1,18 @@
+def interest() :
+    m, y = map(int, input().split())
+
+    dp = [0 for i in range(y+1)]
+    dp[0] = m
+
+    for i in range(1, y+1) :
+        if i >= 5 :
+            dp[i] = max(dp[i-1] * 1.05, dp[i-3] * 1.2, dp[i-5] * 1.35)
+        elif i >= 3 :
+            dp[i] = max(dp[i-1] * 1.05, dp[i-3] * 1.2)
+        else :
+            dp[i] = dp[i-1] * 1.05
+        dp[i] = int(dp[i])
+
+    print(dp[-1])
+
+interest()
