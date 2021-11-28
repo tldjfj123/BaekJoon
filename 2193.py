263fab7c85ab1c117c844b6874707d90271d56d5
tldjfj123 <dksckd0804@naver.com>
2021-11-28 16:08:25 +0900

    21.11.28

---
+++ 2193.py
@@ -0,0 +1,15 @@
+def pn() :
+    n = int(input())
+    dp = [0 for _ in range(n+1)]
+    
+    for i in range(n+1) :
+        if i == 1 :
+            dp[i] = 1
+        elif i == 2 :
+            dp[i] = 1
+        else :
+            dp[i] = dp[i-1] + dp[i-2]
+    
+    print(dp[-1])
+
+pn()
