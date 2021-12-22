43bc3e11b6813037595ef50388235e2628a26b8f
tldjfj123 <dksckd0804@naver.com>
2021-12-22 20:16:29 +0900

    21.12.22

---
+++ 1547.py
@@ -0,0 +1,10 @@
+def ball() :
+    m = int(input())
+    ball = [0, 1, 0, 0]
+    for _ in range(m) :
+        x, y = map(int, input().split())
+        ball[x], ball[y] = ball[y], ball[x]
+    
+    print(ball.index(1))
+         
+ball()
