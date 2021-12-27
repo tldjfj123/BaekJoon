7490b844da5a257aa4e30021d21e5ee3f0b39f11
tldjfj123 <dksckd0804@naver.com>
2021-12-27 14:20:45 +0900

    21.12.27

---
+++ 2523.py
@@ -0,0 +1,12 @@
+def star() :
+    n = int(input())
+    
+    for i in range(1, n+1) :
+        print('*' * i, end = '')
+        print()
+    
+    for i in range(1, n) :
+        print('*' * (n- i), end = '')
+        print()
+            
+star()
