8853a1c549fde3ef0f4deaefec8fb38c89111a61
tldjfj123 <dksckd0804@naver.com>
2022-01-28 12:42:49 +0900

    22.01.28

---
+++ 11286.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+def solution() :
+    n = int(sys.stdin.readline())
+    heap = []
+    
+    for _ in range(n) :
+        x = int(sys.stdin.readline())
+        if x == 0 :
+            if len(heap) == 0 :
+                print(0)
+            else :
+                print(heapq.heappop(heap)[1])
+        else :
+            heapq.heappush(heap, (abs(x), x))
+
+solution()
